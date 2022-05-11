671d9807f3c257527b196fa8ce5312e2110789a0
AyobamiAdebesin <froshofficial@gmail.com>
2022-05-11 03:06:10 -0700

    Added shell prerequisites

---
+++ pre_shell/main.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+
+
+
+#endif
