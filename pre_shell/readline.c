03092a8736b06d0d06f1ee47702d922c75c33a75
AyobamiAdebesin <froshofficial@gmail.com>
2022-05-11 05:29:29 -0700

    Strtok: command line to av

--- pre_shell/readline.c
+++ pre_shell/readline.c
@@ -4,12 +4,15 @@ int main(void)
 {
 	char *buffer = NULL;
 	size_t n = 0;
-	size_t n_bytes_read;
+	int n_bytes_read;
 
-	n_bytes_read = getline(&buffer, &n, stdin);
-
-	printf("Number of characters read: %lu\n", n_bytes_read);
-	printf("%s", buffer);
+	do
+	{
+		write(STDOUT_FILENO, "$ ", 2);
+		n_bytes_read = getline(&buffer, &n, stdin);
+		printf("Number of characters read: %i\n", n_bytes_read);
+		printf("%s", buffer);
+	}while(n_bytes_read != EOF);
 
 	free(buffer);
 
