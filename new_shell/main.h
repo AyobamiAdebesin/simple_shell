9331ec7b3bb20adba03ddc3469c045842a690b8e
AyobamiAdebesin <froshofficial@gmail.com>
2022-05-13 15:37:26 +0100

    Added header file

---
+++ new_shell/main.h
@@ -0,0 +1,17 @@
+#ifndef _main_h_
+#define _main_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <limits.h>
+#include <errno.h>
+
+
+
+
+#endif
