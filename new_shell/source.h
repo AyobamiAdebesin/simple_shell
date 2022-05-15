feee16b0a2782074548ab10218391fa42dc19a32
AyobamiAdebesin <froshofficial@gmail.com>
2022-05-15 16:31:58 -0700

    Added header file source.h

---
+++ new_shell/source.h
@@ -0,0 +1,20 @@
+#ifndef SOURCE_H
+#define SOURCE_H
+
+#define EOF (-1)
+#define ERRCHAR (0)
+#define INIT_SRC_POS (-2)
+
+struct source_s
+{
+	char *buffer;
+	long bufsize;
+	long curpos;
+};
+
+char next_char(struct source_s *src);
+void unget_char(struct source_s *src);
+char peek_char(struct source_s *src);
+void skip_white_spaces(struct source_s *src);
+
+#endif
