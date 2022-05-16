5a95648fb42e4477014690d3243d6af7aa6f93f2
AyobamiAdebesin <froshofficial@gmail.com>
2022-05-16 01:33:47 +0100

    added new function

--- new_shell/read_cmd.c
+++ new_shell/read_cmd.c
@@ -55,3 +55,30 @@ char *read_cmd(void)
 	return ptr;
 }
 
+int parse_and_execute(struct source_s *src)
+{
+    skip_white_spaces(src);
+
+    struct token_s *tok = tokenize(src);
+
+    if(tok == &eof_token)
+    {
+        return 0;
+    }
+
+    while(tok && tok != &eof_token)
+    {
+        struct node_s *cmd = parse_simple_command(tok);
+
+        if(!cmd)
+        {
+            break;
+        }
+
+        do_simple_command(cmd);
+        free_node_tree(cmd);
+        tok = tokenize(src);
+    }
+
+    return 1;
+}
