c2fd0d9a55c50479f841dc348d00a9cdf203b09e
AyobamiAdebesin <froshofficial@gmail.com>
2022-05-19 01:28:23 +0100

    rewrite _realloc.c

--- _realloc.c
+++ _realloc.c
@@ -1,39 +1,27 @@
 #include "main.h"
+
 /**
- * _realloc - reallocates a memory block using malloc and free
- * @ptr: void pointer
- * @old_size: unsigned int
- * @new_size: unsigned int
- *
- * Return: pointer
+ * _realloc - Reallocates a memory block using malloc and free
+ * @old_size: Size of the initial memory block
+ * @new_size: Size of the new memory block
+ * @ptr: Pointer to the previous block
+ * Return: Pointer to the new memory
  */
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *ret = 0;
-	char *write, *read;
 
-	if (new_size == old_size)
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (ptr == NULL)
+		ptr = malloc(new_size);
+	if (new_size ==  old_size)
 		return (ptr);
-
-	if (new_size > 0 || ptr == 0)
-	{
-		ret = malloc(new_size);
-		if (ret == 0)
-			return (0);
-	}
-	if (new_size > 0 && ptr != 0)
-	{
-		write = ret;
-		read = ptr;
-		if (new_size < old_size)
-			old_size = new_size;
-		while (old_size)
-		{
-			old_size--;
-			*write++ = *read++;
-		}
-	}
-
 	free(ptr);
-	return (ret);
+	ptr = malloc(new_size);
+	return (ptr);
 }
